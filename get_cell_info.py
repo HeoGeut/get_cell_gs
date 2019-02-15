066a32cf5ec6613fac4373b100758991c262dbc6
HeoGeut <44108930+HeoGeut@users.noreply.github.com>
2019-02-15 19:06:31 +0900

    add source code
    go on and on

--- get_cell_info.py
+++ get_cell_info.py
@@ -1,1 +1,25 @@
 import time
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+
+scope =  ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'uipath_key1.json', scope)
+
+gc = gspread.authorize(credentials)
+
+
+
+ss_id = '1N8aoWfR47zBa_p6w6JrsH7qYegcbkrZITGShYVaQ8jo'
+
+
+# wks = gc.open(sheet_name).sheet1
+# wks.update_cell()
+
+wks = gc.open("Where is the money Lebowski?").sheet1
+
+wks.update_acell('B2', "it's down there somewhere, let me take another look.")
+
+a = wks.acell('B1').value
+print(a)
