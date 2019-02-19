946c2ba05a531a9de737408ee788d81a87b11819
HeoGeut <44108930+HeoGeut@users.noreply.github.com>
2019-02-19 14:56:32 +0900

    modify sourcecode
    modify sourcecode

--- get_cell_info.py
+++ get_cell_info.py
@@ -10,16 +10,15 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(
 gc = gspread.authorize(credentials)
 
 
-
 ss_id = '1N8aoWfR47zBa_p6w6JrsH7qYegcbkrZITGShYVaQ8jo'
 
 
 # wks = gc.open(sheet_name).sheet1
 # wks.update_cell()
 
-wks = gc.open("Where is the money Lebowski?").sheet1
+wks = gc.open('test').worksheet('test_sheet')
 
 wks.update_acell('B2', "it's down there somewhere, let me take another look.")
 
-a = wks.acell('B1').value
+a = wks.acell('B2').value
 print(a)
