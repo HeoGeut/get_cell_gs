dbe6391252888e2629c2ce21d003ace15682fc32
HeoGeut <44108930+HeoGeut@users.noreply.github.com>
2019-02-20 17:43:01 +0900

    modify source code
    well

--- get_cell_info.py
+++ get_cell_info.py
@@ -2,23 +2,21 @@ import time
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
-scope =  ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
+#구글 스프레드 시트 권한 취득
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
     'uipath_key1.json', scope)
-
 gc = gspread.authorize(credentials)
 
 
-ss_id = '1N8aoWfR47zBa_p6w6JrsH7qYegcbkrZITGShYVaQ8jo'
-
+# ss_id = '1N8aoWfR47zBa_p6w6JrsH7qYegcbkrZITGShYVaQ8jo'
 
 # wks = gc.open(sheet_name).sheet1
 # wks.update_cell()
 
-wks = gc.open('test').worksheet('test_sheet')
+wks = gc.open('test').worksheet('4')
+list_of_lists = wks.get_all_values()
+# wks.update_acell('B2', "it's down there somewhere, let me take another look.")
 
-wks.update_acell('B2', "it's down there somewhere, let me take another look.")
-
-a = wks.acell('B2').value
-print(a)
+# a = wks.acell('B2').value
+print(list_of_lists)
